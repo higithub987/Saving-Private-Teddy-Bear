0bf533f976bd883dae19a0351fcc98d08eb0df91
brody-b9801 <brodymb2@illinois.edu>
2025-10-06 10:12:58 -0500

    Added vase interactable
    vase can be interacted with to push over, shatters - example interactable for showcase

--- Saving Private Teddy Bear/Assets/Scripts/InteractableBehavior.cs
+++ Saving Private Teddy Bear/Assets/Scripts/InteractableBehavior.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class InteractableBehavior : MonoBehaviour
@@ -11,6 +12,26 @@ public class InteractableBehavior : MonoBehaviour
         if (objectName == "tester")
         {
             Destroy(gameObject);
+        }
+        else if (objectName == "vase")
+        {
+            Rigidbody rb;
+            rb = gameObject.AddComponent<Rigidbody>();
+            rb.AddTorque(Vector3.right * 20, ForceMode.Impulse);
+            StartCoroutine(ChangeVase());
+        }
+    }
+
+    IEnumerator ChangeVase()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(GetComponent<Rigidbody>());
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<MeshCollider>().enabled = false;
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
         }
     }
 }
