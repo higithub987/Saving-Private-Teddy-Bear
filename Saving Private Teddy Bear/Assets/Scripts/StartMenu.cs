1409d3fb4d99220d8e3aee84c904cb6d39b65a61
higithub987 <leojiang246@gmail.com>
2025-10-12 16:04:59 -0500

    trying to integrate gamemanager

---
+++ Saving Private Teddy Bear/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StartMenu : MonoBehaviour
+{
+<<<<<<< Updated upstream
+
+    public GameManager manage;
+=======
+>>>>>>> Stashed changes
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void EndGame()
+    {
+        Application.Quit();
+    }
+}
