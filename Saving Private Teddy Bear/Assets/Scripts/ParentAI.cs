4e2561001ec76d88e57673d3392a612569eec6e8
higithub987 <leojiang246@gmail.com>
2025-12-04 20:54:43 -0600

    coding done for vision

--- Saving Private Teddy Bear/Assets/Scripts/ParentAI.cs
+++ Saving Private Teddy Bear/Assets/Scripts/ParentAI.cs
@@ -12,7 +12,13 @@ public class ParentAI : MonoBehaviour
     public float distractionDelayScaling = 2.0f;
 
     [SerializeField]
-    private NavMeshAgent parent; 
+    private NavMeshAgent parent;
+    [SerializeField]
+    private float xmax, ymax;
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private LayerMask playerLayer;
 
     //private stuff
     private bool activeTracking = false;
@@ -34,6 +40,7 @@ public class ParentAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //tracking stuff here
         activeTracking = !(GameManager.distractions.Count == 0);
         //this is highest priority, do everything else in else if
         if (cleaning)
@@ -83,6 +90,24 @@ public class ParentAI : MonoBehaviour
             {
                 cleanupDelay = GameManager.distractions[MaxValObj].Item1 * distractionDelayScaling;
                 cleaning = true;
+            }
+        }
+
+        //win/lose stuff starts here
+
+        float xdist = player.transform.position.x - transform.position.x;
+        float ydist = player.transform.position.y - transform.position.y;
+        if(xdist <= xmax && ydist <= ymax)
+        {
+            Vector3 dir = (player.transform.position - transform.position).normalized;
+            Debug.DrawRay(transform.position, dir, Color.red, Mathf.Sqrt(xmax * xmax + ymax * ymax));
+            if(Physics.Raycast(transform.position, dir, Mathf.Sqrt(xmax * xmax + ymax * ymax), playerLayer)){
+                //manager.loseGame();
+                Debug.Log("lose");
+            }
+            else
+            {
+                return;
             }
         }
     }
