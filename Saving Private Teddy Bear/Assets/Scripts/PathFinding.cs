8c7370d24cec5098219d84f75c370df8791bfb30
SofiaAugusta <113477174+SofiaAugusta@users.noreply.github.com>
2025-12-10 01:09:55 -0600

    implemented NavMesh, created placeholder parent prefab for testing, made pathfinding script (not implemented yet)

---
+++ Saving Private Teddy Bear/Assets/Scripts/PathFinding.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PathFinding : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // track to player ... 
+        // agent.SetDestination(something);
+    }
+}
