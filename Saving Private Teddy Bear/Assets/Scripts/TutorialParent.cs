9518725086f2c9ed027bb1e82812a8210213e4c2
higithub987 <leojiang246@gmail.com>
2025-12-11 00:09:00 -0600

    temp parent changes

---
+++ Saving Private Teddy Bear/Assets/Scripts/TutorialParent.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TutorialParent : MonoBehaviour
+{
+    public GameManager manager;
+
+    public float distanceCutoff = 10.0f;
+    public float cleanupDelay;
+    public float distractionDelayScaling = 2.0f;
+
+    [SerializeField]
+    private NavMeshAgent parent;
+    [SerializeField]
+    private float xmax, ymax;
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private LayerMask playerLayer;
+
+    //private stuff
+    private bool activeTracking = false;
+    private bool trackingObj = false;
+    private bool cleaning = false; //jank way to stop the thing if it's cleaning without coroutines
+    private Vector3 MaxValObj;
+    private float timer = 0f;
+
+    private void Awake()
+    {
+        manager = GameObject.Find("Gamemanager").GetComponent<GameManager>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cleaning)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= cleanupDelay)
+            {
+                cleaning = false;
+                Destroy(GameManager.distractions[MaxValObj].Item2);
+                GameManager.distractions.Remove(MaxValObj);
+            }
+            return;
+        }
+        else if (activeTracking)
+        {
+            if (!trackingObj)
+            {
+                float maxval = 0f;
+                foreach (Vector3 key in GameManager.distractions.Keys)
+                {
+                    float tempval = Vector3.Distance(transform.position, key)
+                        * GameManager.distractions[key].Item1;
+                    if (maxval < tempval)
+                    {
+                        maxval = tempval;
+                        MaxValObj = key;
+                    }
+                }
+                trackingObj = true;
+                return;
+            }
+            else
+            {
+                parent.SetDestination(MaxValObj);
+                foreach (Vector3 key in GameManager.distractions.Keys)
+                {
+                    float tempval = Vector3.Distance(transform.position, key)
+                        * GameManager.distractions[key].Item1;
+                    if (distanceCutoff < tempval)
+                    {
+                        MaxValObj = key;
+                    }
+                }
+            }
+            if (transform.position == MaxValObj)
+            {
+                cleanupDelay = GameManager.distractions[MaxValObj].Item1 * distractionDelayScaling;
+                cleaning = true;
+            }
+        }
+    }
+
+
+}
